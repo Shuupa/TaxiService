decfe9e43fdded70f10ac28a1df49cdfdf4c7e16
Shuupa <shuupach@gmail.com>
2024-04-02 17:56:42 +0700

    BD Cars

---
+++ Taxio/Taxio/Cars.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taxio
+{
+    public class Cars
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID {  get; set; }
+        public string Car_Stat { get; set; }
+
+    }
+}
