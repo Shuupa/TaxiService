0e62e3f7f62fcec4eaf83f743a9c009df5b1e1d5
3321 <1212>
2024-04-10 18:53:35 +0700

    Work

--- Taxio/Taxio/MainPage.xaml.cs
+++ Taxio/Taxio/MainPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using static Xamarin.Essentials.AppleSignInAuthenticator;
 
+[assembly: ExportFont("Impact-Regular.ttf", Alias = "Impact")]
+[assembly: ExportFont("Consola-Reguar.ttf", Alias = "Consola")]
 namespace Taxio
 {
     public partial class MainPage : ContentPage
@@ -14,12 +18,49 @@ namespace Taxio
         public MainPage()
         {
             InitializeComponent();
-            
-
         }
-        protected override void OnAppearing()
+        public double DistanceTimeMath(double distance)
         {
-            Eco_Picker.Text = "0 ₽";
+            double distanceto = distance;
+            double kmtime = 1.50;
+            double TimeToPass = distanceto * kmtime;
+            return TimeToPass;
+        }
+        public void EconomButton_Click(object sender, EventArgs e)
+        {
+            OrderGoButton.BackgroundColor = Color.DarkGreen;
+            int km = 24;
+            double ratemultipler = 1.22;
+            double distance = 25;
+            string PriceMath = distance * km * ratemultipler + " ₽";
+            double TimeToPass =  DistanceTimeMath(distance);
+            TimeToTravel.Text = Convert.ToString("В пути: " + distance + " Км" + " / " + TimeToPass + " Минут");
+            OrderPrice.Text = "";
+            OrderPrice.Text = PriceMath; 
+        }
+        public void BusinessButton_Click(object sender, EventArgs e)
+        {
+            OrderGoButton.BackgroundColor = Color.DarkBlue;
+            int km = 66;
+            double ratemultipler = 2.33;
+            double distance = 35;
+            string PriceMath = distance * km * ratemultipler + " ₽";
+            double TimeToPass = DistanceTimeMath(distance);
+            TimeToTravel.Text = Convert.ToString("В пути: " + distance + " Км" + " / " + TimeToPass + " Минут");
+            OrderPrice.Text = "";
+            OrderPrice.Text = PriceMath;
+        }
+        public void ComfortButton_Click(object sender, EventArgs e)
+        {
+            OrderGoButton.BackgroundColor = Color.FromHex("#FFBE8F56");
+            int km = 32;
+            double ratemultipler = 1.87;
+            double distance = 42;
+            string PriceMath = distance * km * ratemultipler + " ₽";
+            double TimeToPass = DistanceTimeMath(distance);
+            TimeToTravel.Text = Convert.ToString("В пути: " + distance + " Км" + " / " + TimeToPass + " Минут");
+            OrderPrice.Text = "";
+            OrderPrice.Text = PriceMath;
         }
     }
 }
